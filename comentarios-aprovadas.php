f234ace7c63c4ff80e84c42238622563cc7c1423
Rafael Rocha <125105329+rafaelrmello@users.noreply.github.com>
2024-05-29 19:59:26 -0300

    Criação da tela do administrador

---
+++ comentarios-aprovadas.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentários Aprovados</title>
+    <link rel="stylesheet" href="Estilos/adm.css">
+</head>
+<body>
+
+    <div class="sidebar">
+        <div class="admin-section">
+            <h2>Portal do Administrador</h2>
+        </div>
+        <div class="suggestions-section">
+            <h2><a href="adm.php">Comentários em Aguardo</a><br></h2>
+        </div>
+        <div class="suggestions-section">
+            <h2><a href="comentarios-aprovadas.php">Comentários Aprovados</a><br><br></h2>
+        </div>
+        <div class="suggestions-section">
+            <h2><a href="index.html">Sair</a><br><br></h2>
+        </div>
+    </div>
+
+    <div class="main-content">
+        <div class="header">
+            Comentários Aprovados
+        </div>
+
+        <table border="1">
+            <tr>
+                <th>E-mail</th>
+                <th>Comentário</th>
+            </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </table>
+    </div>
+</body>
+</html>
