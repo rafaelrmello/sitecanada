985f1e162f0c25c3ea7c0b5353158242ddd7d3a9
Rafael Rocha <125105329+rafaelrmello@users.noreply.github.com>
2024-05-28 21:13:58 -0300

    Criação da tela de cadastro e o back-end para o registro no banco de dados

---
+++ usuario-gravar.php
@@ -0,0 +1,20 @@
+<?php
+try {
+    $usuario = $_POST ["usuario"];
+    $email = $_POST ["email"];
+    $senha = hash ("sha256", $_POST["senha"]);
+
+
+    $sql = "INSERT INTO usuario (usuario, email, senha) VALUES ('{$usuario}', '{$email}', '{$senha}')";
+
+    include_once "classes/conexao.php";
+    $conexao->exec($sql);
+    echo "<h3>Usuário registrado com sucesso</h3>";
+    echo "<a href='login.html'>Fazer login </a>";
+}
+catch (Exception $erro) {
+    echo $erro->getMessage();
+    echo "Ocorreu um erro";
+}
+
+?>
